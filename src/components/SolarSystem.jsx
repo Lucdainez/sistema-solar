25983181826bc044115ce449959a2a98e17cccdd
Lucas Dainez <lucdainez@gmail.com>
2022-07-20 16:38:58 -0300

    feat(SolarSystem): adicionar planetas
    aqui foi renderizado um planeta por vez no componente SolarSystem a partir de um map feito em um array com objetos passados para o componente PlanetCard

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
+    const onePlanet = planets.map((planet) => (<PlanetCard
+      key={ planet.name }
+      planetName={ planet.name }
+      planetImage={ planet.image }
+    />));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {onePlanet}
       </div>
     );
   }
