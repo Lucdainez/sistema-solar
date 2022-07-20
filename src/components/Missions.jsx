bd316d1dfbc013403e13f3b45a236cc53658c07c
Lucas Dainez <lucdainez@gmail.com>
2022-07-20 17:14:11 -0300

    feat(Missions): renderizar componente
    dentro do componente Missions foi importado um array de objetos onde foi feito um map para passar cada objeto individualmente para o componente MissionsCard e renderizado dentro de Missions

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    const oneMission = missions.map((mission) => (<MissionCard
+      key={ mission.name }
+      name={ mission.name }
+      year={ mission.year }
+      country={ mission.country }
+      destination={ mission.destination }
+    />));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { oneMission }
       </div>
     );
   }
