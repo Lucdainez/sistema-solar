355c3e5f3ba4b59609bd19909c9e6f553e06c015
Lucas Dainez <lucdainez@gmail.com>
2022-07-20 19:59:18 -0300

    style(App): Css3
    todas as letras estão brancas e o background da página está mudado

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div className='reactBack'>
         <Header />
         <SolarSystem />
         <Missions />
-      </>
+      </div>
     );
   }
 }
